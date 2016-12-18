9107cd88e050249a255d56a615dd67b468e97dce
neozack <neozack@gmail.com>
2016-12-18 14:41:04 +0000

    Added initial container class and references to SimpleInjector.

---
+++ PairingTest.Web/Middleware/DependencyInjection/Container.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PairingTest.Web.Middleware.DependencyInjection
+{
+    internal class Container
+    {
+    }
+}
